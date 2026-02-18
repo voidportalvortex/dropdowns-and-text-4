e4b4508a48c2846ceba4fcb2fbd86d76475017e2
voidportalvortex <meitriton@gmail.com>
2026-02-17 19:18:01 -0500

    sounds and music
    i forgot sounds and music

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from 'react'
 import fairyLight from './assets/fairyNormal.gif'
 import fairyDark from './assets/dark.gif'
+import bgMusic from './assets/78-Sun Room (Alone with Relaxing Tea).mp3'
+import apiMagic from './assets/yoba.wav'
+import typing from './assets/dialogueCharacter.wav'
 import './App.css'
 
-function TextInput({ text, setText }) {
+function TextInput({ text, setText, aud }) {
   const handleChange = (event) => {
     setText(event.target.value);
+    if(aud){
+      aud.currentTime = 0;
+      aud.play();
+    }
   }
   return (
     <div>
@@ -15,9 +22,12 @@ function TextInput({ text, setText }) {
   )
 }
 
-function CheckBoxes({ check, setCheck }) {
+function CheckBoxes({ check, setCheck, aud }) {
   const handleChange = (event) => {
     setCheck(event.target.checked);
+    if(aud){
+      aud.play();
+    }
   }
   return(
     <div>
@@ -62,32 +72,45 @@ function DropdownComplex({index, setIndex}) {
   )
 }
 
-function IndexToAPI({ lat, setLat, long, setLong, index, stock, setStock, slug, setSlug }) {
+function IndexToAPI({ lat, setLat, long, setLong, index, stock, setStock, slug, setSlug, aud }) {
   //console.log(index);
   if (index === 1){
     return (
       <div>
         <p>whats the stock symbol?</p>
-        <TextInput text = {stock} setText = {setStock} id = "symbol" />
+        <TextInput text = {stock} setText = {setStock} id = "symbol" aud = {aud} />
       </div>
     )
   }else if(index === 2){
     return (
       <div>
         <p>whats the slug (this is complex dw)?</p>
-        <TextInput text = {slug} setText = {setSlug} id = "slug" />
+        <TextInput text = {slug} setText = {setSlug} id = "slug" aud = {aud} />
       </div>
     )
   }else if(index === 0){
     return(
       <div>
         <p> whats the latitude for ur location?</p>
-        <TextInput text = {lat} setText = {setLat} id = "latitude" />
+        <TextInput text = {lat} setText = {setLat} id = "latitude" aud = {aud} />
         <p> whats the longitude for ur location?</p>
-        <TextInput text = {long} setText = {setLong} id = "longitude" />
+        <TextInput text = {long} setText = {setLong} id = "longitude" aud = {aud} />
       </div>
     )
   }
+}
+
+function Background({ music, musicLoaded, setMusicLoaded }) {
+  useEffect(() => {
+    if (music && !musicLoaded) {
+      music.loop = true;
+      music.volume = 0.5;
+      music.play();
+      setMusicLoaded(true);
+    }
+  }, [music, musicLoaded, setMusicLoaded]);
+  
+  return null; // This component doesn't render anything
 }
 
 function App() {
@@ -113,6 +136,21 @@ function App() {
   const workMessage = work ? `i work as a ${work}` : 'i dont have a job :( im unemployed';
   const nepoBaby = !work && !(broke) ? 'so therefore im a nepo baby' : null;
 
+  const [bgm, setBgm] = useState(null);
+  const [typingAudio, setTypingAudio] = useState(null);
+  const [apiAudio, setApiAudio] = useState(null);
+  const [musicLoaded, setMusicLoaded] = useState(false);
+
+  useEffect(() => {
+    const bgmInstance = new Audio(bgMusic);
+    const typingInstance = new Audio(typing);
+    const apiInstance = new Audio(apiMagic);
+    setBgm(bgmInstance);
+    setTypingAudio(typingInstance);
+    setApiAudio(apiInstance);
+  }, [])
+    
+
   useEffect(() => {
     if (dark === true) {
       document.documentElement.style.setProperty('--background-color', '#41394d');
@@ -137,6 +175,7 @@ function App() {
       //console.log(data);
       //console.log(data.current.apparent_temperature);
       //console.log(data.current.temperature_2m);
+      apiAudio.play();
       setMessage(`as a fairy i can tell u the weather and its ${data.current.temperature_2m} degrees fahrenheit but lowkirk it feels like ${data.current.apparent_temperature} degrees fahrenheit`);
     } catch (error) {
       if (error.name === 'AbortError') return;
@@ -153,7 +192,8 @@ function App() {
       )
       const data = await response.json();
       //console.log(data);
-      setMessage(`as a fairy i can tell u the stock price of ${stock} and its currently ${data.c} dollars with a growth of ${data.dp}% since yesterday`)
+      apiAudio.play();
+      setMessage(`as a fairy i can tell u the stock price of ${stock} and its currently ${data.c} dollars with a growth of ${data.dp}% since yesterday`);
     } catch (error) {
       if (error.name === 'AbortError') return;
       console.error('u dont got wifi beta', error);
@@ -169,6 +209,7 @@ function App() {
       //console.log(data);
       //console.log(data[0]);
       //console.log(data[0].lastTradePrice);
+      apiAudio.play();
       const probability = Math.round(data[0].lastTradePrice * 100);
       setMessage(`as a fairy i can tell u polymarket goons think for the question "${data[0].question}" there is a ${probability}% chance of it happening`)
     } catch (error) {
@@ -208,27 +249,28 @@ function App() {
         <p> {moneyMessage} </p>
         <p> {nepoBaby} </p>
         <p> {message} </p>
+        <Background music = {bgm} musicLoaded = {musicLoaded} setMusicLoaded = {setMusicLoaded} />
       </div>
       <div className = "box">
         <p> whats her name? </p>
-        <TextInput text={text} setText={setText} id = "name" />
+        <TextInput text={text} setText={setText} id = "name" aud = {typingAudio} />
         <p> how old is she? </p>
-        <TextInput text={age} setText={setAge} id = "age" />
+        <TextInput text={age} setText={setAge} id = "age" aud = {typingAudio} />
         <p> what does she work as </p>
-        <TextInput text={work} setText={setWork} id = "work" />
+        <TextInput text={work} setText={setWork} id = "work" aud = {typingAudio} />
         <div className = "vertical">
           <div className = "check-container">
             <p> is she broke?</p>
-            <CheckBoxes check={broke} setCheck={setBroke} id = "broke" />
+            <CheckBoxes check={broke} setCheck={setBroke} id = "broke" aud = {typingAudio} />
           </div>
           <div className = "check-container">
             <p> is she dark?</p>
-            <CheckBoxes check={dark} setCheck={setDark} id = "dark" />
+            <CheckBoxes check={dark} setCheck={setDark} id = "dark" aud = {typingAudio} />
           </div>
         </div>
         <br />
         <DropdownComplex index={index} setIndex={setIndex} id = "apis" />
-        <IndexToAPI lat={lat} setLat={setLat} long={long} setLong={setLong} index={index} stock={stock} setStock={setStock} slug={slug} setSlug={setSlug} />
+        <IndexToAPI lat={lat} setLat={setLat} long={long} setLong={setLong} index={index} stock={stock} setStock={setStock} slug={slug} setSlug={setSlug} aud = {typingAudio} />
       </div>
     </div>
   )
